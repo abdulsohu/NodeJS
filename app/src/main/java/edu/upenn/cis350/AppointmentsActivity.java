36c361669b805e44174868e85095b51edd4d8245
abdulmoizmunir <sohu@sas.upenn.edu>
2020-04-26 15:56:09 -0500

    Emailing on appointment acceptance functional

--- app/src/main/java/edu/upenn/cis350/AppointmentsActivity.java
+++ app/src/main/java/edu/upenn/cis350/AppointmentsActivity.java
@@ -1,12 +1,17 @@
 package edu.upenn.cis350;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInstaller;
 import android.media.MediaCas;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Message;
 import android.se.omapi.Session;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,25 +20,30 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.net.PasswordAuthentication;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 public class AppointmentsActivity extends AppCompatActivity {
     Map<String, String> appointments;
-
-    MediaCas.Session session = null;
-    ProgressDialog progressDialog = null;
-    Context context = null;
-    String rec, subject, textMessage;
-
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        context = this;
         appointments = new HashMap<String, String>();
         appointments.put("jack", "pending");
         appointments.put("penny", "accepted");
@@ -44,6 +54,7 @@ public class AppointmentsActivity extends AppCompatActivity {
         Toast.makeText(this, "I can't do it man!", Toast.LENGTH_SHORT).show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_appoitments);
+        queue = Volley.newRequestQueue(this);
 
 //        String json = loadJSONFromAsset("appointmentData.json");
 //        try {
@@ -97,7 +108,7 @@ public class AppointmentsActivity extends AppCompatActivity {
         updateSpinner();
     }
 
-//    @Override
+    //    @Override
 //    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 //        // On selecting a spinner item
 //        String item = parent.getItemAtPosition(position).toString();
@@ -133,7 +144,6 @@ public class AppointmentsActivity extends AppCompatActivity {
         }
 
 
-
         // Creating adapter for spinner
         ArrayAdapter<String> dataAdapterU = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, upcomingAppointment);
         ArrayAdapter<String> dataAdapterP = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, pendingAppointments);
@@ -156,17 +166,38 @@ public class AppointmentsActivity extends AppCompatActivity {
             updateSpinner();
         }
 
-        rec = "sohu@sas.upenn.edu";
-        subject = "Appointment confirmed with:" +  "abdul" + ", for:" + "22 April, 2020";
-        textMessage = "This is a FANYHEALTH confirmation of appointment with" + "abdul";
+        String docEmail = "sohu@sas.upenn.edu";
+        String docName = "Abdul";
+        String patientEmail = "fatiho@seas.upenn.edu";
+        String patientName = "Fatih";
 
-        Properties properties = new Properties();
-        properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", "465");
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.socketFactory.port", "465");//TLS
-        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        sendEmail(docEmail, patientName);
+        sendEmail(patientEmail, docName);
+        Toast.makeText(this, "Email confirmation sent!", Toast.LENGTH_LONG).show();
+    }
+
+    public void sendEmail(String email, String appointmentWith) {
+        StringBuffer route = new StringBuffer("http://10.0.2.2:3000/mail/");
+        route.append(email + "/");
+        route.append(appointmentWith);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, route.toString(),
+                null,
+
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+        queue.add(request);
     }
 
     public void onRejectButtonClick(View v) {
@@ -186,10 +217,7 @@ public class AppointmentsActivity extends AppCompatActivity {
         //Intent i = new Intent(this, EditAppointment.class);
         //i.putExtra("MESSAGE", "HI");
         //startActivityForResult(i, 1);
-
-
     }
-
 
 
 }
